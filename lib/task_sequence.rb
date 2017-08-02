a00a929bce2e1a774363967fef36a77b54784427
modus <illuminatiday@gmail.com>
2017-08-02 11:02:33 +0300

    #007 Adding new test

--- lib/task_sequence.rb
+++ lib/task_sequence.rb
@@ -1,44 +1,46 @@
-class TaskSequence
+  class TaskSequence
 
-	def initialize(steps)
-		@steps = steps
-		@row = '1'
-		@sequence = Array.new << "1"
-	end
+  	def initialize(steps)
+  		@steps = steps
+  		@row = "1"
+  		@sequence = ["1"]
+  	end
 
-	def step(row)
-		@row = row
-		@row = @row.to_s
-		next_digit = Array.new
-		row_length = @row.length
-		@n = 0
-		@count = 1
-		while @n < row_length
-			digit = @row[@n]			
-			if digit == @row[@n+1]				
-				@count = @count + 1
-				@n = @n +1	
-				 
-			else
-				@n = @n + 1
-				next_digit << @count.to_s + digit
-				@count = 1
-			end
-		end
-			@row = next_digit.join("")
-			return @row
-	end
+  	def sequence()
+  	 @steps = @steps.to_i
+  	  while @steps != 1
+  		  @sequence << (step(@row)) 
+  		  @steps = @steps - 1			
+  		end		
+  	 return @sequence
+  	end 
 
-	def sequence()
-		@steps = @steps.to_i
-	    while @steps != 0
-		@sequence << (step(@row)) 
-		@steps = @steps - 1			
-		end		
-		return @sequence
-	end
+    private 
 
-end
+    def step(row)
+      @row = row
+      @row = @row.to_s
+      next_digit = Array.new
+      row_length = @row.length
+      @n = 0
+      @count = 1
+      while @n < row_length
+        digit = @row[@n]      
+        if digit == @row[@n+1]        
+          @count = @count + 1
+          @n = @n +1  
+           
+        else
+          @n = @n + 1
+          next_digit << @count.to_s + digit
+          @count = 1
+        end
+      end
+        @row = next_digit.join("")
+        return @row
+    end
+
+  end
 
 
 
--- sequence.rb
+++ sequence.rb
@@ -2,12 +2,14 @@ require File.join(%w[. lib task_sequence])
 
 @user_step_sequence = 0
 @result_of_sequence = Array.new
-puts "Please enter number of steps (1 - 50)"
+print "Please enter number of steps (1 - 50) - "
 @user_step_sequence = gets.chomp
 new_sequence = TaskSequence.new(@user_step_sequence)
-if (Integer(@user_step_sequence)rescue nil).nil? || (Integer(@user_step_sequence) > 50)
+
+if (Integer(@user_step_sequence)rescue nil).nil? || (Integer(@user_step_sequence) > 50) || (Integer(@user_step_sequence) <= 0)
 	puts "Wrong number of steps!"
 	return 1
 end
-@rezult = new_sequence.sequence
-puts @rezult
+
+@result = new_sequence.sequence
+puts @result
